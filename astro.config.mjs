486ab2d466a748b4eb2b09d1ff951396c1fa2932
sir-mr-bean <kroucher.1019@gmail.com>
2022-08-22 05:38:28 +1000

    remove vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+// import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
   // vite: {
   //   ssr: {
   //     external: ["svgo"],
