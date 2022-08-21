99b604cd19c58bc65c2a06bb66e36d661584817b
sir-mr-bean <kroucher.1019@gmail.com>
2022-08-22 04:57:07 +1000

    refactor sections to .astro, update icons to astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  // vite: {
-  //   ssr: {
-  //     external: ["svgo"],
-  //   },
-  // },
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
