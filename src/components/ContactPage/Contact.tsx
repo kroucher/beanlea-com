5f528df466bb751d724c01dc0f3f28d56f6391b4
sir-mr-bean <kroucher.1019@gmail.com>
2022-08-22 08:01:58 +1000

    add contact form submit feedback

--- src/components/ContactPage/Contact.tsx
+++ src/components/ContactPage/Contact.tsx
@@ -38,6 +38,7 @@ type Inputs = {
 export default function ContactPage() {
   const [needs, setNeeds] = useState<string | undefined>();
   const [emailSent, setEmailSent] = useState(false);
+  const [sendingEmail, setSendingEmail] = useState(false);
   const {
     register,
     handleSubmit,
@@ -56,18 +57,24 @@ export default function ContactPage() {
     const result = await trigger(undefined, {
       shouldFocus: true,
     });
+    console.log(result);
     console.log(JSON.stringify(values, null, 2));
-    try {
-      const emailResult = await fetch("/api/submit", {
-        method: "POST",
-        body: JSON.stringify(values, null, 2),
-      });
+    if (result) {
+      setSendingEmail(true);
+      try {
+        const emailResult = await fetch("/api/submit", {
+          method: "POST",
+          body: JSON.stringify(values, null, 2),
+        });
 
-      if (emailResult.ok) {
-        setEmailSent(true);
+        if (emailResult.ok) {
+          setEmailSent(true);
+          setSendingEmail(false);
+        }
+      } catch (error) {
+        setSendingEmail(false);
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
     }
   };
 
@@ -233,7 +240,34 @@ export default function ContactPage() {
                       onClick={onSubmit}
                       className="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-slate-900 bg-blue-400 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
                     >
-                      Submit
+                      {sendingEmail ? (
+                        <div className="flex items-center">
+                          <svg
+                            id="spinner"
+                            className="animate-spin spinner -ml-1 mr-3 h-5 w-5 text-slate-900 dark:text-slate-50/70 z-20"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle
+                              className="opacity-25"
+                              cx="12"
+                              cy="12"
+                              r="10"
+                              stroke="currentColor"
+                              strokeWidth="4"
+                            ></circle>
+                            <path
+                              className="opacity-75"
+                              fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                          </svg>
+                          <span>Sending...</span>
+                        </div>
+                      ) : (
+                        "Send"
+                      )}
                     </button>
                   </div>
                 </form>
