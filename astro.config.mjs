1b23a5967cbc53abafe5a4f49528a712945e2ad4
sir-mr-bean <kroucher.1019@gmail.com>
2022-08-23 08:21:22 +1000

    add partytown + plausible

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,22 @@ import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    sitemap({
-      customPages: [
-        "https://www.beanlea.com/about",
-        "https://www.beanlea.com/contact",
-        "https://www.beanlea.com/showcase",
-        "https://www.beanlea.com/",
-      ],
-    }),
-    robotsTxt(),
-  ],
+  integrations: [tailwind(), react(), sitemap({
+    customPages: ["https://www.beanlea.com/about", "https://www.beanlea.com/contact", "https://www.beanlea.com/showcase", "https://www.beanlea.com/"]
+  }), robotsTxt(), partytown()],
   site: "https://www.beanlea.com",
   output: "server",
   adapter: vercel(),
   experimental: {
-    integrations: true,
-  }, // vite: {
+    integrations: true
+  } // vite: {
   //   ssr: {
   //     external: ["svgo"],
   //   },
   // },
+
 });
