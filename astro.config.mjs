e43a058177310acaab3fdc73ced471bdd8a5242f
sir-mr-bean <kroucher.1019@gmail.com>
2022-08-22 11:38:40 +1000

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   output: "server",
-  adapter: vercel(),
-  // vite: {
+  adapter: vercel() // vite: {
   //   ssr: {
   //     external: ["svgo"],
   //   },
   // },
+
 });
