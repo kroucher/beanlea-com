5042b4e2a88888b03f8a7cc8fab396ccbd97e4be
kroucher <kroucher.1019@gmail.com>
2022-09-08 08:41:39 +1000

    fix: add loading spinner

--- src/components/Showcase/ShowcaseTwo.tsx
+++ src/components/Showcase/ShowcaseTwo.tsx
@@ -106,6 +106,14 @@ function Model({ open, hinge, ...props }: {
     )
 }
 
+function Spinner() {
+    return <Html center className="h-screen w-screen flex items-center justify-center">
+        <svg className="animate-spin -ml-1 mr-3 h-5 sm:h-10 w-5 sm:w-10 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+    </Html>;
+}
 
 const ShowcaseTwo = () => {
     const [open, setOpen] = useState(true)
@@ -113,7 +121,7 @@ const ShowcaseTwo = () => {
     return (
 
         <Canvas dpr={[1, 2]} camera={{ position: [0, 0, -60], fov: 35 }}>
-            <Suspense fallback={null}>
+            <Suspense fallback={<Spinner />}>
                 <ambientLight intensity={0.4} />
                 <directionalLight position={[0, 5, 10]} />
                 <PresentationControls
